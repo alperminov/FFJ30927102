1ebb5cd78d6e98671639fe98b63febb5264ee3fd
lexeyko <lexeykozz@gmail.com>
2017-04-23 13:16:56 +0300

    Добавлена обработка клавиши I для вызова инвентаря

---
+++ Assets/Scripts/Menu/PlayerMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMenu : MonoBehaviour {
+
+	bool inventoryIsActive;
+	Canvas canvas;
+	// Use this for initialization
+	void Start () {
+		canvas = GetComponent<Canvas> ();
+		canvas.enabled = false;
+		inventoryIsActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("i")) {
+			if (inventoryIsActive) {
+				canvas.enabled = false;
+				inventoryIsActive = false;
+			}
+			else {
+				canvas.enabled = true;
+				inventoryIsActive = true;
+			}
+		}
+	}
+}
